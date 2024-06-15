5c6c0230319024e9de7c0ab9e523191ce0856094
Lucas Sorge <lucasorge13@gmail.com>
2024-06-15 08:34:08 -0500

    update bubble sort

--- src/sortingAlgorithms/mergeSort.js
+++ src/sortingAlgorithms/mergeSort.js
@@ -77,26 +77,3 @@ function doMerge(
 }
 
 
-
-export function getBubbleSortAnimations(array) {
-  const animations = [];
-  if (array.length <= 1) return array;
-  bubbleSort(array, animations);
-  return animations;
-}
-
-function bubbleSort(array, animations) {
-  const n = array.length;
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      animations.push([j, j + 1, 'compare']);
-      if (array[j] > array[j + 1]) {
-        animations.push([j, j + 1, 'swap']);
-        [array[j], array[j + 1]] = [array[j + 1], array[j]];
-      }
-      animations.push([j, j + 1, 'revert']);
-    }
-  }
-}
-
-
